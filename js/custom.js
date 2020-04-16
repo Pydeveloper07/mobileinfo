4228aeef92c3a3a6aa06503d66686de08faa0324
Tukhtamurod Isroilov <inha07111999@gmail.com>
2020-04-16 17:23:45 +0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+//Preloader
+// jQuery(window).load(function () {
+//     $('#load').delay(500).fadeOut('slow');
+//     $('#preloader').delay(900).fadeOut('slow');
+//     $('body').delay(900).css({
+//         'overflow': 'visible',
+//     });
+// });
