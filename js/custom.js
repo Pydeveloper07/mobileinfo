8d58740bbe5a4793d3442b9e15b8d580732d4aaa
Tukhtamurod Isroilov <inha07111999@gmail.com>
2020-04-17 21:02:22 +0500

    finished featured listings sub menus

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,23 @@
 //         'overflow': 'visible',
 //     });
 // });
+jQuery(document).ready(function(){
+    var sideMenuHeight = $(window).height() - parseInt($('.side-menu').css('margin-top').substring(0, $('.side-menu').css('margin-top').length - 1));
+    var rightMenuHeight = $(window).height() - parseInt($('.right-menu').css('margin-top').substring(0, $('.right-menu').css('margin-top').length - 1));
+    $('.side-menu').css({
+        'max-height': sideMenuHeight + 'px',
+    })
+    $('.right-menu').css({
+        'max-height': rightMenuHeight + 'px',
+    })
+    $(window).resize(function(){
+        var sideMenuHeight = $(window).height() - parseInt($('.side-menu').css('margin-top').substring(0, $('.side-menu').css('margin-top').length - 1));
+        $('.side-menu').css({
+            'max-height': sideMenuHeight + 'px',
+        })
+        var rightMenuHeight = $(window).height() - parseInt($('.right-menu').css('margin-top').substring(0, $('.right-menu').css('margin-top').length - 1));
+        $('.right-menu').css({
+            'max-height': rightMenuHeight + 'px',
+        })
+    })
+});
